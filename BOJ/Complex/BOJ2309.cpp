a9390fd42d1ee9118fac1871d16114a56108b954
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-03-13 02:04:38 +0900

    BOJ2309: 일곱난쟁이

---
+++ BOJ/Complex/BOJ2309.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a[9];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for(int i = 0; i < 9; i++) {
+        int temp;
+        cin >> temp;
+        if(temp < 100) {
+            a[i] = temp;
+        } else return 0;
+    }
+    sort(a, a+9);
+    
+    do {
+        int sum = 0;
+        for(int i = 0; i <7; i++) sum += a[i];
+
+        if(sum == 100) break;
+    }while(next_permutation(a,a+9));
+
+    for(int i = 0; i < 7; i++) {
+        cout << a[i] << "\n";
+    }
+
+    return 0;
+
+
+}
