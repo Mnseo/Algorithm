9bec9028c4f87c348c47d9829d2c85751a74ff5d
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-03-16 02:56:05 +0900

    BOJ: 14729 칠무해

--- BOJ/Greedy/BOJ14729.cpp
+++ BOJ/Greedy/BOJ14729.cpp
@@ -1,3 +1,29 @@
 #include <bits/stdc++.h>
 //백준 14729 - 칠무해
 using namespace std;
+
+typedef unsigned long long ll;
+const int INF = 987654321;
+int n;
+double temp;
+priority_queue<double> pq;
+vector<double> v;
+
+int main() {
+    cin >> n;
+    for(int i =0; i < n; i++) {
+        cin >> temp;
+        if(pq.size() == 7) {
+            pq.push(temp);
+            pq.pop();
+        } else pq.push(temp);
+    }
+    while(pq.size()) {
+        v.push_back(pq.top());
+        pq.pop();
+    }
+
+    reverse(v.begin(),v.end());
+    for(double i : v) printf("%.3lf\n", i);
+    return 0;
+}
---
+++ BOJ/Greedy/BOJ2170.cpp
@@ -0,0 +1,3 @@
+#include <bits/stdc++.h>
+//백준 2170 선긋기 (12:52)
+using namespace std;
