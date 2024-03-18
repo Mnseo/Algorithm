6d7dd945f8bcff9dd7979de5df3d4298f591737e
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-03-18 17:04:48 +0900

    BOJ12100: 2048 게임

---
+++ BOJ/Greedy/BOJ12100.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+//백준 삼성SW역량 12100 2048게임
+using namespace std;
+const int max_n =24;
+int ret, n;
+
+struct Board {
+    int a[max_n][max_n];
+    void _rotate90() {
+        int temp[max_n][max_n];
+        for(int i =0; i<n; i++) {
+            for(int j =0; j < n; j++) {
+                temp[i][j] = a[n-j-1][i];
+            }
+        }
+        memcpy(a, temp, sizeof(a));
+    }
+
+    void _move() {
+        int temp[max_n][max_n];
+        for(int i = 0; i < n; i++) {
+            int c = -1, d = 0;
+            for(int j = 0; j < n; j++) {
+                if(a[i][j] ==0) continue;
+                if(d && a[i][j] == temp[i][c]) temp[i][c] *=2, d = 0;
+                else temp[i][++c] = a[i][j], d = 1;
+            }
+            for(c++; c < n; c++) temp[i][c] = 0;
+        }
+        memcpy(a, temp, sizeof(a));
+    }
+
+    void get_max() {
+        for(int i = 0; i< n; i++) {
+            for(int j = 0; j < n; j++) {
+                ret = max(ret, a[i][j]);
+            }
+        }
+    }
+};
+
+void go(Board c, int here) {
+    if(here == 5) {
+        c.get_max();
+        return;
+    }
+    for(int i = 0; i < 4; i++) {
+        Board d = c;
+        d._move();
+        go(d, here +1);
+        c._rotate90();
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    Board c;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> c.a[i][j];
+        }
+    }
+
+    go(c,0);
+    cout << ret << "\n";
+    return 0;
+}
---
+++ BOJ/Greedy/BOJ3190.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+//백준 삼성SW역량 12100 2048게임
+using namespace std;
+
+const int max_n =24;
+int n, a[max_n][max_n], ny,nx;
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+int main() {
+    scanf("%d", &n);
+    for(int i =0; i<n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%1d", &a[i][j]);
+        }
+    }
+
+    for(int i = 0; i < 4; i++) {
+        int
+    }
+
+    return;
+}
