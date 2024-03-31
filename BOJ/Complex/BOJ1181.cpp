322384d54a61eb36f3a9de7e782738ea8498d262
MinseoShindor <tlsalstj01@duksung.ac.kr>
2024-03-31 11:24:49 +0900

    BOJ1181: 단어정렬

---
+++ BOJ/Complex/BOJ1181.cpp
@@ -0,0 +1,32 @@
+#include<string>
+#include<algorithm>
+#include<iostream>
+// 백준 1181 단어 정렬
+using namespace std;
+
+int cmp(string a, string b) {
+    if(a.length() == b.length()) {
+        return a < b;
+    } else {
+        return a.length() < b.length();
+    }
+}
+
+string v[20000];
+
+int main() {
+    int a;
+    cin >> a;
+    vector<string> v(a,"");
+    for(int i = 0; i < a; i++) {
+        cin >> v[i];
+    }
+
+    sort(v, v+a, cmp);
+
+    for(int i = 0; i < a; i++) {
+        if(v[i] == v[i-1]) continue;
+        else cout << v[i] << '\n';
+        
+    }
+}
--- BOJ/Graph/BOJ1992.cpp
+++ BOJ/Graph/BOJ1992.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+#include<cstdio>
+#include<algorithm>
+#include<string>
 //BOJ 1992 쿼드트리
 using namespace std;
 
